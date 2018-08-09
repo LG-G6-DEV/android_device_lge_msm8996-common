791dec55003345a030c7d971d7796e3754e1fa8d
Imaginovskiy <imaginovskiy@kilo-tango.net>
2018-08-09 19:23:16 +0000

    Add msm8996 compatible pocketmode from Xiaomi

--- msm8996.mk
+++ msm8996.mk
@@ -158,6 +158,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiDoze
 
+# Pocket Mode
+PRODUCT_PACKAGES += \
+    XiaomiPocketMode
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
