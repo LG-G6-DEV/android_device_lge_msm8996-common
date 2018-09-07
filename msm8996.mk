b8c61cfd5465b1c6e5cb63c906d37fd5981d8c82
Imaginovskiy <imaginovskiy@kilo-tango.net>
2018-09-07 13:41:55 +0000

    USB: Remove unnecessary USB HAL

--- msm8996.mk
+++ msm8996.mk
@@ -355,11 +355,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service \
-    com.android.future.usb.accessory
-
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
