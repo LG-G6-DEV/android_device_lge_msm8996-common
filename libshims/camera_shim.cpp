360e81145a4d2692a7178179c0ec95f7e0c1b469
Alexis <alexisr245@gmail.com>
2019-01-02 22:12:20 +0100

    feat: update shim camera

--- libshims/camera_shim.cpp
+++ libshims/camera_shim.cpp
@@ -48,3 +48,7 @@ extern "C" void _ZN7android20DisplayEventReceiverC1ENS_16ISurfaceComposer11Vsync
 extern "C" void _ZN7android20DisplayEventReceiverC1Ev() {
     _ZN7android20DisplayEventReceiverC1ENS_16ISurfaceComposer11VsyncSourceE;
 }
+
+extern "C" void _ZN14SeeMoreWrapper11_zkfJx38iRJEb();
+
+extern "C" void _ZN7android5FenceD1Ev();
