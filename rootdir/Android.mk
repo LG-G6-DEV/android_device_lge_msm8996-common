e3323187aeb86278ca51d7cc501c73081dcfb640
Alexis <alexisr245@gmail.com>
2019-01-02 19:00:55 +0100

    fix: bt no addr

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -74,6 +74,14 @@ LOCAL_MODULE_RELATIVE_PATH := init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.program_bdaddr.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES    := bin/init.program_bdaddr.sh
+LOCAL_VENDOR_MODULE    := true
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.qcom.rc
 LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS  := optional eng
---
+++ rootdir/bin/init.program_bdaddr.sh
@@ -0,0 +1,2 @@
+#!/vendor/bin/sh
+setprop ro.boot.btmacaddr "00:00:00:00:00:00"
