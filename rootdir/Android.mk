b3ccc70b36b0ac84ee3e214762b8dd87b43ccf70
Alexis <alexisr245@gmail.com>
2019-01-10 20:18:19 +0100

    refactor: update sensors init

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -40,14 +40,6 @@ LOCAL_REQUIRED_MODULES := init.radio.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.sensors.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES    := bin/init.qcom.sensors.sh
-LOCAL_VENDOR_MODULE    := true
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.graphics.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := EXECUTABLES
