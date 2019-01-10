b737b5c2b5a0833b71c3ebd434929fa5dce692cf
Alexis <alexisr245@gmail.com>
2019-01-10 17:50:38 +0100

    fix: thermal

--- thermal/thermal-helper.cpp
+++ thermal/thermal-helper.cpp
@@ -50,8 +50,8 @@ static unsigned int gVrThrottledBelowMin;
  */
 bool initThermal() {
     std::string hardware = android::base::GetProperty("ro.hardware", "");
-    if (hardware == "mata") {
-        LOG(ERROR) << "Initialization on Mata";
+    if (hardware == "qcom") {
+        LOG(ERROR) << "Initialization on QCom";
         gSkinSensorNum = kSkinSensorNum;
         gSkinSensorType = kSkinSensorType;
         gTsensOffset = kTsensOffset;
