56a92d6ce429776ff11c3cda7c71d1207f29d246
Alexis <alexisr245@gmail.com>
2019-01-16 23:22:25 +0100

    feat: add privapp-permissions-qti

--- msm8996.mk
+++ msm8996.mk
@@ -303,8 +303,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
-# Qualcomm broadcast whitelist
+# Qualcomm
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
 # RCS
