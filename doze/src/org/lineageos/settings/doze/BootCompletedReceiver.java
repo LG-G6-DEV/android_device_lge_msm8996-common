8dd9b34014e72e62ede708cd44d7e44c6b6f6b0b
Alexis <alexisr245@gmail.com>
2019-01-25 14:24:50 +0100

    refactor: LGDoze

--- doze/src/org/lineageos/settings/doze/BootCompletedReceiver.java
+++ doze/src/org/lineageos/settings/doze/BootCompletedReceiver.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2015 The CyanogenMod Project
+ * Copyright (C) 2015 The CyanogenMod Project
+ *               2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +15,7 @@
  * limitations under the License.
  */
 
-package com.custom.ambient.display;
+package org.lineageos.settings.doze;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -24,7 +25,7 @@ import android.util.Log;
 public class BootCompletedReceiver extends BroadcastReceiver {
 
     private static final boolean DEBUG = false;
-    private static final String TAG = "XiaomiDoze";
+    private static final String TAG = "LGDoze";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
