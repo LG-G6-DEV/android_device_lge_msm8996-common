8b48e5cbf468025771b63d593f6d060300f2180a
Alex <alexisr245@gmail.com>
2019-01-28 16:54:44 +0100

    fix: remove comment

--- msm8996.mk
+++ msm8996.mk
@@ -88,7 +88,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.common@4.0-util \
     android.hardware.audio.effect@4.0 \
     android.hardware.audio.effect@4.0-impl \
-    # android.hardware.soundtrigger@2.1-impl
     audio.a2dp.default \
     audio.primary.msm8996 \
     audio.r_submix.default \
