e671250c2b50fa7882904872e9a30edf5bbe4aff
Michael <wolfovelp@gmail.com>
2019-02-03 19:16:57 +0100

    DRM Update
    Taken from Xiaomi msm8996-common

--- msm8996.mk
+++ msm8996.mk
@@ -173,7 +173,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.1-service.clearkey
 
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
