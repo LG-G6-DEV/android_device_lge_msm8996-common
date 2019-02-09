69c319d819bbbb59d963aaeac6169c0d91c6b903
Alex <alexisr245@gmail.com>
2019-02-09 14:13:39 +0100

    refactor: now included in init scripts

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -191,10 +191,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.NO_STAPA=1 \
     persist.radio.VT_HYBRID_ENABLE=1
 
-# Memory optimizations
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.qti.sys.fw.bservice_enable=true
-
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.sensors.hal=e \
