cabbdae56b6540c2e210e98bfd121e7eec6f4ddb
Michael Bardoň <wolfovelp@gmail.com>
2019-06-07 13:48:03 +0200

    msm8996-common: wifi: add line WIFI_DRIVER_FW_PATH_MFG

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -240,6 +240,6 @@ BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
-
+WIFI_DRIVER_FW_PATH_MFG     := "/vendor/firmware/bcmdhd_mfg.bin"
 # inherit from the proprietary version
 -include vendor/lge/msm8996-common/BoardConfigVendor.mk
