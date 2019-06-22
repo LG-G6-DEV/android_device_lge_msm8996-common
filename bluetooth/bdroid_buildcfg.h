8605a124db6d426b1e36a4426070c5c2fe5e8be9
Michael Bardoň <wolfovelp@gmail.com>
2019-06-22 23:36:11 +0200

    msm8996-common: bluetooth: cleanup of unused variables

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -42,7 +42,6 @@ inline const char* BtmGetDefaultName()
 #define MAX_ACL_CONNECTIONS 16
 #define MAX_L2CAP_CHANNELS 16
 #define BLE_VND_INCLUDED  TRUE
-#define BTM_WBS_INCLUDED TRUE /* Enable WBS */
 #define BTIF_HF_WBS_PREFERRED TRUE /* Use WBS */
 
 // skips conn update at conn completion
