ae8b1e376174bf0f720ffc00cb93bf6edbd7e390
Michael Bardoň <wolfovelp@gmail.com>
2019-09-10 15:51:17 +0200

    msm8996-common: doze: Apply Q changes on AmbientDisplayConfiguration

--- doze/src/org/lineageos/settings/doze/Utils.java
+++ doze/src/org/lineageos/settings/doze/Utils.java
@@ -20,6 +20,7 @@ package org.lineageos.settings.doze;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.hardware.display.AmbientDisplayConfiguration;
 import android.os.UserHandle;
 import android.support.v7.preference.PreferenceManager;
 import android.provider.Settings;
