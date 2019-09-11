ebf3de4557e4b04a75fa10283964a93d3800f201
dom133 <dom133_pl@protonmail.com>
2019-09-11 17:14:57 +0200

    msm8996-common: [TMP] bt: Remove PROPERTY_VALUE_MAX

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,22 +18,11 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#include <cutils/properties.h>
 #include <string.h>
 
 inline const char* BtmGetDefaultName()
 {
-	char product_name[PROPERTY_VALUE_MAX];
-	property_get("ro.product.name", product_name, "");
-
-	if (strstr(product_name, "h1"))
-		return "LG G5";
-	if (strstr(product_name, "elsa"))
-		return "LG V20";
-	if (strstr(product_name, "lucye"))
-		return "LG G6";
-
-	return "";
+	return "LG G6";
 }
 
 #define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
@@ -64,5 +53,4 @@ inline const char* BtmGetDefaultName()
 #define BTHW_FW_EXTENDED_CONFIGURATION TRUE
 #define BTHW_FW_EXTENDED_CONFIGURATION_ONLY_I2SPCM_CONFIG FALSE
 
-#undef PROPERTY_VALUE_MAX
 #endif
