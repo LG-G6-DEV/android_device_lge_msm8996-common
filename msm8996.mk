4e936224f511d6f9ccfb8e3044b9dab7b1eb4e89
dom133 <dom133_pl@protonmail.com>
2019-09-16 21:45:58 +0200

    msm8996-common: Add move_widevine_data.sh

--- msm8996.mk
+++ msm8996.mk
@@ -183,7 +183,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.2-service.clearkey \
+    move_widevine_data.sh
 
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
