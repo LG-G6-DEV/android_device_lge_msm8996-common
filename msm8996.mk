38f943235ea40819b92360bf1d683766a4083a0e
dom133 <dom133_pl@protonmail.com>
2020-07-03 11:24:29 +0200

    msm8996-common: Drop powerhint.xml
    * We don't use it

--- msm8996.mk
+++ msm8996.mk
@@ -380,10 +380,6 @@ PRODUCT_PACKAGES += \
     libstagefrighthw \
     libstagefright_soft_flacdec
 
-# Power
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
-
 # RCS
 PRODUCT_PACKAGES += \
     rcs_service_aidl \
