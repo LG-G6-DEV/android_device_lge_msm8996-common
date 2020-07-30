9d40d4132854ca597c8b95589beabd8265b0211f
Michael Bardoň <wolfovelp@gmail.com>
2020-07-30 21:49:06 +0200

    msm8996-common: loggy: Use .log instead of .txt
    * looks nicer :)

--- rootdir/bin/loggy.sh
+++ rootdir/bin/loggy.sh
@@ -2,7 +2,7 @@
 # loggy.sh.
 
 _date=`date +%F_%H-%M-%S`
-logcat -b all -f  /cache/logcat_${_date}.txt &
-dmesg -w > /cache/kmsg_${_date}.txt &
+logcat -b all -f  /cache/logcat_${_date}.log &
+dmesg -w > /cache/kmsg_${_date}.log &
 
 exit 0
