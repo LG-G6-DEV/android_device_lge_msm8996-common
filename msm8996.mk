110c4728f133c9336124e963f87f72c6ad84cad7
Michael Bardoň <wolfovelp@gmail.com>
2020-08-02 15:27:51 +0200

    msm8996-common: Remove deprecated trust-zone check
    * We dont need this thingy

--- msm8996.mk
+++ msm8996.mk
@@ -394,10 +394,6 @@ PRODUCT_PACKAGES += \
     rcs_service_api \
     rcs_service_api.xml
 
-# Recovery
-PRODUCT_PACKAGES += \
-    librecovery_updater_msm8996
-
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
