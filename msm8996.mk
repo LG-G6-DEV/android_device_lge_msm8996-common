09ddb6941818ccf6d97a75e513e553e82b9b3d42
dom133 <dom133_pl@protonmail.com>
2020-08-13 00:10:14 +0200

    msm8996-common: Build GPS from source

--- msm8996.mk
+++ msm8996.mk
@@ -238,17 +238,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_GMS_CLIENTID_BASE := android-om-lg
 
 # GPS
-PRODUCT_PACKAGES += \
-    android.hardware.gnss@2.0-impl-qti:64 \
-    android.hardware.gnss@2.0-service-qti \
-    libbatching \
-    libgeofencing \
-    libgnss
+$(call inherit-product, hardware/qcom/gps/gps_vendor_board.mk)
+$(call inherit-product, hardware/qcom/gps/gps_vendor_product.mk)
 
+PRODUCT_PACKAGES += \
+    libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
-    $(LOCAL_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
     $(LOCAL_PATH)/gps/etc/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
     $(LOCAL_PATH)/gps/etc/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
     $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
