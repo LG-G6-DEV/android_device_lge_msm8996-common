a374091b8bfa8fbafea2ee5719a4025d51d3b3f6
Alessandro Astone <ales.astone@gmail.com>
2020-09-16 00:55:25 +0200

    msm8996-common: Build health hal
     * It is required in android-11
    
    Change-Id: I229326167ea8cb7083760d96700204cf9151d7f2
    
    Conflicts:
    	manifest.xml

--- msm8996.mk
+++ msm8996.mk
@@ -271,6 +271,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
     android.hardware.health@2.0-service
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
