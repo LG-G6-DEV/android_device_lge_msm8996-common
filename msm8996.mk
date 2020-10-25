4e09b3a434c4e2fd32ff9cc5f260de3dac9d7e26
Alessandro Astone <ales.astone@gmail.com>
2020-10-25 22:36:24 +0100

    msm8996-common: Move camera provider HAL to passthrough
     * Camera devices were getting discovered WAY too late,
       after android had dispatched the BOOT_COMPLETED broadcast,
       which made Camera2 mark itself as disabled.
    
    Change-Id: I1a282f99a98330ad1a8dadded9e69734136d7469
    
    Conflicts:
    	manifest.xml

--- msm8996.mk
+++ msm8996.mk
@@ -128,7 +128,6 @@ TARGET_SCREEN_WIDTH := 1440
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
     Snap
 
