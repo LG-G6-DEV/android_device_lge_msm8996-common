adaec8cdb19391cb4a5a2374e4d676249f78c5c3
dom133 <dom133_pl@protonmail.com>
2020-10-31 09:52:36 +0100

    msm8996-common: Remove old health hal

--- msm8996.mk
+++ msm8996.mk
@@ -267,11 +267,6 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
-
-# Health
-PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
